fb469fe1ca685897270d07047926d9771258a073
Gene Diaz <genediazjr@gmail.com>
2020-03-22 10:32:01 +0800

    include util to tests

--- test/index.js
+++ test/index.js
@@ -3,15 +3,16 @@
 const Code = require('@hapi/code');
 const Lab = require('@hapi/lab');
 const Lib = require('..');
+const Util = require('../lib/util');
 
 const expect = Code.expect;
 const lab = exports.lab = Lab.script();
 const describe = lab.describe;
 const it = lab.it;
 
-describe('lib', () => {
+describe('phcovid19', () => {
 
-    it('loads', async () => {
+    it('lib.load', async () => {
 
         const data = await Lib.load();
         expect(data.stat_meta).exist();
@@ -23,5 +24,23 @@ describe('lib', () => {
         expect(data.case_profiles).exist();
         expect(data.case_hospitals).exist();
         expect(data.case_residences).exist();
+    });
+
+    it('util.fixFacilityName', () => {
+
+        expect(Util.fixFacilityName(0)).to.equal(0);
+    });
+
+    it('util.fixResidence', () => {
+
+        expect(Util.fixResidence(0)).to.equal(0);
+    });
+
+    it('util.increment', () => {
+
+        expect(() => {
+
+            Util.increment(1, {});
+        }).to.not.throw();
     });
 });
